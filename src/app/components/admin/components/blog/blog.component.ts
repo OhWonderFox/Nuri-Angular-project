792467c55cfa50e5f7f2c862cca08c1f12349394
Chris Bordeman <cbordeman@outlook.com>
2023-01-11 09:33:16 -0500

    added class for blog type

--- src/app/components/admin/components/blog/blog.component.ts
+++ src/app/components/admin/components/blog/blog.component.ts
@@ -4,29 +4,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  styleUrls: ['./blog.component.css'],
 })
 export class BlogComponent {
+  blogs: Array<blogType>;
 
-  constructor() { }
-  blogs = []
-  ngOnInit() {
-
+  constructor() {
+    this.blogs = new Array<blogType>();
   }
-  addBlog(title: any,content: any) {
+  ngOnInit() {}
+  addBlog(title: any, content: any) {
     //console.log(title.value,content.value);
     //let blog = { "title": any; "content": any }[] = [];
 
-    let blog = {"title": title.value, "content": content.value};
-    if(localStorage.getItem("blogs")){
-      this.blogs =  JSON.parse(localStorage.getItem("blogs")!)
+    let blog = new blogType(title.value, content.value);
+    if (localStorage.getItem('blogs')) {
+      this.blogs = JSON.parse(localStorage.getItem('blogs')!);
     }
-    this.blogs.push(blog)
-    localStorage.setItem("blogs",JSON.stringify(this.blogs))
-    title.value = ""
-    content.value = ""
-    alert("Blog Added!")
-
+    this.blogs.push(blog);
+    localStorage.setItem('blogs', JSON.stringify(this.blogs));
+    title.value = '';
+    content.value = '';
+    alert('Blog Added!');
   }
+}
+export class blogType {
+  title: string;
+  content: string;
 
+  constructor(title: string, content: string) {
+    this.title = title;
+    this.content = content;
+  }
 }
