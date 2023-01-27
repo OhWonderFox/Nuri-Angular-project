e38da4a0474421da5fac3cdde63291ded6392bda
Nuri <nurbalakairatovna@gmail.com>
2023-01-27 14:36:47 +0400

    cmt

--- src/app/components/pages/components/game/game.component.ts
+++ src/app/components/pages/components/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver } from '@angular/core';
 
 @Component({
   selector: 'app-game',
@@ -24,6 +24,7 @@ export class GameComponent {
     this.isDraw = '';
     this.counter = 0;
     this.freshpage = false;
+    this.xIsNext = true;
   }
 
   get player() {
@@ -35,6 +36,42 @@ export class GameComponent {
       this.squares.splice(idx,1,this.player)
       this.xIsNext = !this.xIsNext;
       this.counter++;
+    }
+  
+    this.checkWinner();
+
+    if(!this.winner && this.counter == 9) {
+      this.newGame();
+    }
+
+  }
+
+  checkIndicesEqual(){
+
+    const wins = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    
+    for(let i=0;i<wins.length;i++) {
+      const [idx1,idx2,idx3] = wins[i];
+      if(this.squares[idx1] && this.squares[idx1] === this.squares[idx2] && this.squares[idx1] === this.squares[idx3]){
+        return this.squares[idx1];
+      }
+    }
+    return null;
+  }
+
+  checkFor3(val: string): boolean {
+    if(this.checkIndicesEqual() === val){
+      return true;
+    }
+      return false;
+  }
+
+  checkWinner(){
+    if(this.checkFor3('X') === true ){
+      this.winner = 'X';
+    }
+    else if(this.checkFor3('O') === true){
+      this.winner = 'O';
     }
   }
 }
